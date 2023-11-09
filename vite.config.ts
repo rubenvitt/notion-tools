8ca85bd4cc50a01c79d9073bc3ecb87da8a01597
Ruben Vitt <dev@rubeen.dev>
2023-11-09 09:06:11 +0100

    update packages & fix api

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import vue from '@vitejs/plugin-vue'
-import ssr from 'vike/plugin'
+import vercel from 'vite-plugin-vercel'
 import {UserConfig} from 'vite'
+import {ssr} from "vike/dist/esm/node/plugin";
+import {vitePluginSsrVercelPlugin} from "@vite-plugin-vercel/vike";
 
 const config: UserConfig = {
-  plugins: [vue(), ssr()],
+    plugins: [vue(), ssr(), vercel(), vitePluginSsrVercelPlugin()],
 }
 
 export default config
